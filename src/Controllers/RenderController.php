e4c50ef9772d9b37cd45bcd5ea0035d61d7c8e90
Marguillat <erwanduchene@gmail.com>
2025-02-04 23:47:20 +0100

    rien

--- src/Controllers/RenderController.php
+++ src/Controllers/RenderController.php
@@ -4,20 +4,22 @@ namespace src\Controllers;
 
 /**
  * Main render hold global rendering variables
+ *blabal
  */
-abstract class RenderController {
-
+abstract class RenderController
+{
     public $baseUrl;
 
     protected function __construct()
     {
-       $this->baseUrl = self::makeBaseUrl(); 
+        $this->baseUrl = self::makeBaseUrl();
     }
 
-    private function makeBaseUrl(){
+    private function makeBaseUrl(): string
+    {
         $protocol = $_SERVER["REQUEST_SCHEME"];
         $servHost = $_SERVER["HTTP_HOST"];
 
-        return $protocol.'://'.$servHost;
+        return $protocol . "://" . $servHost;
     }
 }
