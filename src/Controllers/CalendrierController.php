153d23f4054ba5953ea9a335e29ee71770b9ac35
Marguillat <erwanduchene@gmail.com>
2025-02-19 18:35:11 +0100

    truc

--- src/Controllers/CalendrierController.php
+++ src/Controllers/CalendrierController.php
@@ -2,19 +2,17 @@
 
 namespace src\Controllers;
 
-use src\Models\calendrierModel;
+use src\Models\CalendrierModel;
 
-class CalendrierController extends RenderController{
-
+class CalendrierController extends RenderController
+{
     public function __construct()
-    {   
+    {
         parent::__construct();
 
         CalendrierModel::getSessions();
         // dégueulasse à changer
-        require('src/Views/partials/header.phtml');
-        require('src/Views/calendrier.phtml');
+        require "src/Views/partials/header.phtml";
+        require "src/Views/calendrier.phtml";
     }
-
-
 }
