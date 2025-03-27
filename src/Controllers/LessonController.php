541c002b94ad9a51ad95cd9bb83cda3dea7b262f
Marguillat <erwanduchene@gmail.com>
2025-03-27 22:20:15 +0100

    minor indentation

--- src/Controllers/LessonController.php
+++ src/Controllers/LessonController.php
@@ -10,18 +10,24 @@ class LessonController
   public function addLessonDate($moduleId, $date, $timeStart, $timeEnd)
   {
     try {
-      new DataBase;
+      new DataBase();
       $db = DataBase::connect();
-      $query = $db->prepare("INSERT INTO lesson (id_module, date, time_start, time_end) VALUES (:moduleId, :date, :timeStart, :timeEnd)");
-      $query->bindParam(':date', $date);
-      $query->bindParam(':timeStart', $timeStart);
-      $query->bindParam(':timeEnd', $timeEnd);
-      $query->bindParam(':moduleId', $moduleId);
+      $query = $db->prepare(`
+        INSERT
+          INTO lesson
+            (id_module, date, time_start, time_end)
+          VALUES
+            (:moduleId, :date, :timeStart, :timeEnd)`);
+
+      $query->bindParam(":date", $date);
+      $query->bindParam(":timeStart", $timeStart);
+      $query->bindParam(":timeEnd", $timeEnd);
+      $query->bindParam(":moduleId", $moduleId);
       $query->execute();
 
-      return ['success' => true];
+      return ["success" => true];
     } catch (Exception $e) {
-      return ['success' => false, 'message' => $e->getMessage()];
+      return ["success" => false, "message" => $e->getMessage()];
     }
   }
 }
